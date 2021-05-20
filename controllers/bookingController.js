a344a927a686f184bd276bb8f6d197a3eedb57f1
Debajyoti Debbarma <debajyotidebbarma55@gmail.com>
2021-05-21 03:38:24 +0530

    Improved Stripe integration 7

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -59,7 +59,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.amount / 100;
+  const price = session.amount_total / 100;
+
   await Booking.create({ tour, user, price });
 };
 
